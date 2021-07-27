c36b01362b1a898e0f4579c98d62e4e5c60fa290
Jokand <84349256+Jokand@users.noreply.github.com>
2021-07-27 19:57:55 +0300

    refactoring User

--- app/src/main/java/com/pioner/User.kt
+++ app/src/main/java/com/pioner/User.kt
@@ -1,19 +1,22 @@
 package com.pioner
 
+import android.widget.EditText
 import com.google.firebase.database.IgnoreExtraProperties
 
 @IgnoreExtraProperties
-class User {
-    var email: String? = null
-    var name: String? = null
-    var lastname: String? = null
-
-    constructor() {}
-
-    constructor(email: String?, name: String?, lastname: String?) {
-        this.email = email
-        this.name = name
-        this.lastname = lastname
-    }
-
+//class User {
+//    var email: String? = null
+//    var name: String? = null
+//    var lastname: String? = null
+//
+//    constructor() {}
+//
+//    constructor(email: String?, name: String?, lastname: String?) {
+//        this.email = email
+//        this.name = name
+//        this.lastname = lastname
+//    }
+//
+//}
+class User(email: String, name: String, lastname: String) {
 }
