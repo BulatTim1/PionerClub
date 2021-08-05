6b3298b802bb46c57fff67636d13e505dd36718d
semenfbish <lgg320040@gmail.com>
2021-08-05 22:02:58 +0300

    + очистка лишнего кода

--- app/src/main/java/com/pioner/fragments/ConfirmationRegistrationFragment.kt
+++ app/src/main/java/com/pioner/fragments/ConfirmationRegistrationFragment.kt
@@ -8,57 +8,20 @@ import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.pioner.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ConfirmationRegistrationFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ConfirmationRegistrationFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        val root: View = inflater.inflate(R.layout.fragment_confirmation_registration, container, false)
-        val reg_end : Button = root.findViewById(R.id.reg_end)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root: View =
+            inflater.inflate(R.layout.fragment_confirmation_registration, container, false)
+        val reg_end: Button = root.findViewById(R.id.reg_end)
         // Inflate the layout for this fragment
-        reg_end.setOnClickListener{
+        reg_end.setOnClickListener {
             parentFragmentManager.beginTransaction().replace(R.id.login_host, LoginFragment())
                 .commit()
         }
         return root
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ConfirmationRegistrationFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic fun newInstance(param1: String, param2: String) =
-                ConfirmationRegistrationFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
     }
 }
