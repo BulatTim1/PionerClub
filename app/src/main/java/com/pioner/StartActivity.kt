521c535d0e66a01e7fe5ebe72804d2d3bcb3c391
semenfbish <lgg320040@gmail.com>
2021-08-05 23:30:09 +0300

    + castomshap ActionBar

--- app/src/main/java/com/pioner/StartActivity.kt
+++ app/src/main/java/com/pioner/StartActivity.kt
@@ -3,6 +3,7 @@ package com.pioner
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 
 class StartActivity : AppCompatActivity() {
