28a9e336903740f993f4acbf250e0d5c87e2aa02
Jokand <84349256+Jokand@users.noreply.github.com>
2021-08-06 21:31:03 +0300

    закончил дизайн мейн страницы, но булка с корицей сломала стили (почини)

--- app/src/main/java/com/pioner/fragments/MainPageStudentFragment.kt
+++ app/src/main/java/com/pioner/fragments/MainPageStudentFragment.kt
@@ -1,60 +1,35 @@
 package com.pioner.fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.pioner.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MainPageStudentFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MainPageStudentFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_page_student, container, false)
+        val root: View = inflater.inflate(R.layout.fragment_main_page_student, container, false)
+        val progressBar: ProgressBar = root.findViewById(R.id.ProgressCircleBar)
+        val progressText: TextView = root.findViewById(R.id.ProgressCircleText)
+
+        setProgress(57, progressBar, progressText)
+        return root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MainPageStudentFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MainPageStudentFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+    @SuppressLint("SetTextI18n")
+    private fun setProgress(progress: Int, Bar : ProgressBar, Text : TextView) { //можно вставлять значения от 0 до 100
+        val strProgress = "$progress %"
+        Bar.progress = progress
+        Text.text = "Завершено упражнений $strProgress"
     }
+
 }
