db2e696ec54f72eb76dab91fdc02b982b8f9bb55
Jokand <84349256+Jokand@users.noreply.github.com>
2021-08-07 22:42:48 +0300

    попыт ка не пытка

--- app/src/main/java/com/pioner/MeasurementClass.kt
+++ app/src/main/java/com/pioner/MeasurementClass.kt
@@ -1,4 +1,4 @@
 package com.pioner
 
-data class MeasurementClass(var mass: Int? = null, var height: Int? = null, var calories: Int? = null, var Ldt: String? = null) {
+data class MeasurementClass(var mass: Int = 0, var height: Int = 0, var calories: Int = 0, var Ldt: String? = null) {
 }
