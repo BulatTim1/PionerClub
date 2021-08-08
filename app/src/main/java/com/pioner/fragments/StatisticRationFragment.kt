90af7de611b8856dc9d477f5aa6156b3f2b1bb46
Jokand <84349256+Jokand@users.noreply.github.com>
2021-08-08 16:59:08 +0300

    сделал отображение пустоты

--- app/src/main/java/com/pioner/fragments/StatisticRationFragment.kt
+++ app/src/main/java/com/pioner/fragments/StatisticRationFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.view.isEmpty
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
